0a6197b94d7c89d7bdc3874c37feee6e367b1dd2
Jorge Loch <jorgembloch2001@gmail.com>
2024-04-27 01:16:34 -0300

    adding the command to close only one buffer

--- nvim/lua/jorgeloch_/core/keymaps.lua
+++ nvim/lua/jorgeloch_/core/keymaps.lua
@@ -4,15 +4,15 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader>t", "<cmd>TransparentToggle<cr>")
 
-keymap.set("n", "<leader>s-", "<cmd>split<cr>")
-keymap.set("n", "<leader>s\\", "<cmd>vsplit<cr>")
+keymap.set("n", "<leader>c", "<cmd>bd<cr>") -- buffer delete
+keymap.set("n", "<leader>s-", "<cmd>split<cr>") -- horizontal split
+keymap.set("n", "<leader>s\\", "<cmd>vsplit<cr>") -- vertical split
 
-keymap.set("n", "<S-h>", "<cmd>bprevious<cr>")
-keymap.set("n", "<S-l>", "<cmd>bnext<cr>")
-keymap.set("n", "<leader>c", "<cmd>BufferKill<cr>")
+keymap.set("n", "<S-h>", "<cmd>bprevious<cr>") -- move to previous buffer
+keymap.set("n", "<S-l>", "<cmd>bnext<cr>") -- move to next buffer
 
-keymap.set("n", "<leader>w", "<cmd>w<cr>")
-keymap.set("n", "<leader>q", "<cmd>wqa<cr>")
+keymap.set("n", "<leader>w", "<cmd>w<cr>") -- save file
+keymap.set("n", "<leader>q", "<cmd>wqa<cr>") -- close all buffers
 
 keymap.set("n", "Esc", "<cmd>noh<cr>")
 keymap.set("n", "U", "<cmd>redo<cr>")
